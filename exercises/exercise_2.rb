ffc8995b7ace745462cf4f0d372d0cfffbd11475
Robert Shum <robertshum@gmail.com>
2023-11-13 22:23:11 -0800

    finish e2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,10 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store1.name = "North Vancouver"
+@store1.save
+@store1 = Store.find_by(id: 1)
+p @store1
 # Your code goes here ...
