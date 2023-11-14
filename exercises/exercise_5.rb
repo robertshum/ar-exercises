c8a9902e177c9b419915d859df19a925b063674f
Robert Shum <robertshum@gmail.com>
2023-11-13 22:44:32 -0800

    finish e5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "total revenue #{Store.sum("annual_revenue")}"
+puts "avg revenue #{Store.average("annual_revenue")}"
+
+puts "# of stores w/ more than $1M in an. revenue #{Store.where("annual_revenue > ?", 1000000).count}"
