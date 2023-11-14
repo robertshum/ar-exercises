6ec799a8ce10a138e05bd3281c2d043926def677
Robert Shum <robertshum@gmail.com>
2023-11-13 22:35:45 -0800

    finish e4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,23 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+Store.create(name: "Whistler", annual_revenue: 1900000,  mens_apparel: true, womens_apparel: false)
+
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+# only men's apparel
+@mens_store = Store.where(mens_apparel: true)
+
+# loop and print name and revenue
+@mens_store.each do |store|
+  print "#{store.name} #{store.annual_revenue} \n"
+end
+
+# only women's apparel
+@womens_store = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+
+@womens_store.each do |store|
+  print "#{store.name} #{store.annual_revenue} \n"
+end
