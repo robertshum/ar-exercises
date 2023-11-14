ee7680259bb83a1b8f9e8c74c4f1a83695d156db
Robert Shum <robertshum@gmail.com>
2023-11-13 22:53:50 -0800

    finish e6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Rob", last_name: "Shum", hourly_rate: 70)
+
+
+@store2.employees.create(first_name: "Joe", last_name: "Blow", hourly_rate: 80)
+@store2.employees.create(first_name: "Yolo", last_name: "Swag", hourly_rate: 90)
